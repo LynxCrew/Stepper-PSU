9036cbd61841dd3ac8891d39c5324a3942e77d2a
Zeanon <thezeanon@gmail.com>
2025-07-15 13:56:18 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Stepper-PSU
+# LynxCrew Stepper-Brake Plugin
+
+## What does it do:
+This plugin enables a stepper_brake config section in Kalico/Klipper if you are
+using either an Electromagnetic Stepper Brake or a pcb that shorts out the
+phases of your steppers to increase the detent torque.
+If configured, it will enable a defined output pin immediately after drivers are turned on
+(to disengage the brake) and disable it immediately before drivers are turned
+off (to engage the break)
+order of operations still has to be discussed!
+
+## Install:
+SSH into you pi and run:
+```
+cd ~
+wget -O - https://raw.githubusercontent.com/LynxCrew/Stepper-Brake/psu_enable/install.sh | bash
+```
+
+then add this to your moonraker.conf:
+```
+[update_manager stepper-brake]
+type: git_repo
+channel: dev
+path: ~/stepper-brake
+origin: https://github.com/LynxCrew/Stepper-Brake.git
+managed_services: klipper
+primary_branch: psu_enable
+install_script: install.sh
+```
+
+## How to use:
+!!This does only work if your drivers have an enable pin as it hooks directly
+into the enable pin code!!
+(on the other hand, drivers without an enable pin are always active and thus
+should not need a brake, if you feel like you still need it, open an issue and
+I will try to implement it)
+just add
+```
+[stepper_psu my_stepper_psu]
+pin: 
+#   the pin to be switched when steppers turn on or off
+wait_time: 0.0
+#   The Time to block after enabling steppers so the psu has time to fully turn on
+stepper:
+#   the steppers that should cause the pin to switch.
+#   If this parameter is not specified, the module will listen to all steppers
+```
+
+`DISABLE_STEPPER_PSU STEPPER_PSU=my_stepper_psu` will turn it off again
---
+++ source/stepper_psu.py
@@ -0,0 +1,60 @@
+class StepperBrakeEnablePin:
+    def __init__(self, enable, stepper_brake):
+        self.enable = enable
+        self.stepper_brake = stepper_brake
+        self.mcu_pin = stepper_brake.mcu_pin
+        self.toolhead = stepper_brake.toolhead
+        self.wait_time = stepper_brake.wait_time
+        self.mcu_enable = self.enable.mcu_enable
+        self.enable.mcu_enable = self
+
+    def set_digital(self, print_time, value):
+        if value:
+            self.mcu_pin.set_digital(print_time, value)
+            self.toolhead.dwell(self.wait_time)
+        self.mcu_enable.set_digital(print_time, value)
+
+
+class StepperPSU:
+    def __init__(self, config):
+        self.config = config
+        self.full_name = config.get_name()
+        self.name = self.full_name.split()[-1]
+        self.printer = config.get_printer()
+        self.toolhead = self.printer.lookup_object("toolhead")
+        ppins = self.printer.lookup_object("pins")
+        self.mcu_pin = ppins.setup_pin("digital_out", config.get("pin"))
+        self.stepper_names = config.getlist("stepper", None)
+        self.wait_time = config.getfloat("wait_time", 0.0, minval=0.0)
+        self.stepper_enable = self.printer.load_object(config, "stepper_enable")
+        self.printer.register_event_handler("klippy:ready", self._handle_ready)
+        self.printer.register_event_handler("klippy:connect", self._handle_connect)
+
+        gcode = self.printer.lookup_object("gcode")
+        gcode.register_mux_command(
+            "DISABLE_STEPPER_PSU",
+            "STEPPER_PSU",
+            self.name,
+            self.cmd_DISABLE_STEPPER_PSU,
+            desc=self.cmd_DISABLE_STEPPER_PSU_help,
+        )
+
+    def _handle_connect(self):
+        all_steppers = self.stepper_enable.get_steppers()
+        if self.stepper_names is None:
+            self.stepper_names = all_steppers
+
+    def _handle_ready(self):
+        for stepper_name in self.stepper_names:
+            StepperBrakeEnablePin(
+                self.stepper_enable.lookup_enable(stepper_name).enable,
+                self,
+            )
+
+    def cmd_DISABLE_STEPPER_PSU(self, gcmd):
+        systime = self.printer.get_reactor().monotonic()
+        print_time = self.mcu.estimated_print_time(systime)
+        self.mcu_pin.set_digital(print_time, 0)
+
+def load_config_prefix(config):
+    return StepperPSU(config)
